215638b5184a7388e715af9c420cc5d5b4d7dd2d
Brandon Chan <brandonchan45@gmail.com>
2022-12-30 20:24:40 -0800

    added functionality for the bot to randomly choose a restaurant, updated code to show the user's score, updated data struct so users can correctly add multiple restaurants to be considered

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import sys
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -15,7 +16,10 @@ if not TOKEN:
 # creates a bot object
 bot = discord.Bot()
 
-Options = []
+Options = {}
+
+Score = {}
+
 
 @bot.event
 async def on_ready():
@@ -26,23 +30,66 @@ async def on_ready():
 @bot.slash_command(name="addrestaurant", guild_ids=[1032437764614012988],
                    description="Adds a restaurant to be chosen from. Users can add as many restaurants as they want.")
 async def addRestaurant(interaction: discord.Interaction, restaurant):
-    Options.append(restaurant)
+    if interaction.user.id not in Options:
+        Options[interaction.user.id] = restaurant
+    elif isinstance(Options[interaction.user.id], list):
+        Options[interaction.user.id].append(restaurant)
+    else:
+        Options[interaction.user.id] = [Options[interaction.user.id], restaurant]
+
     await interaction.response.send_message(f'{interaction.user} added {restaurant} to the choices!')
+
+
+@bot.slash_command(name="random", guild_ids=[1032437764614012988],
+                   description="Have the bot randomly pick a restaurant from the list of options.")
+async def randomChoice(interaction: discord.Interaction):
+
+    # picks a random restaurant submitted by a user from the options dictionary
+    restaurants = list(Options.values())
+    randNum: int = random.randint(0, len(restaurants) - 1)
+    name: str = restaurants[randNum]
+
+    # if the value is a list, pick a random restaurant from the list
+    if isinstance(restaurants[randNum], list):
+        output: int = random.randint(0, len(restaurants[randNum]) - 1)
+        name = restaurants[randNum][output]
+
+    if interaction.user.id not in Score:
+        Score[interaction.user.id] = 1
+    else:
+        Score[interaction.user.id] += 1
+
+    await interaction.response.send_message('The restaurant chosen is: ' + name)
 
 
 # slash command to see how many wins a particular user has
 @bot.slash_command(name="score", guild_ids=[1032437764614012988],
                    description="Returns the number of times a user has chosen the restaurant the group goes to.")
 async def score(interaction: discord.Interaction):
-    await interaction.response.send_message(f'{interaction.user} has chosen the restaurant ___ times!')
+
+    global userScore
+    for key in Score:
+        if interaction.user.id == key:
+            userScore = Score[interaction.user.id]
+            break
+        else:
+            userScore = 0
+
+    if userScore == 1:
+        grammar = 'time'
+    else:
+        grammar = 'times'
+    await interaction.response.send_message(f'{interaction.user} has chosen the restaurant {userScore} {grammar}!')
 
 
 @bot.slash_command(name="options", guild_ids=[1032437764614012988], description="Returns the valid options that users "
                                                                                 "have submitted")
 async def options(interaction: discord.Interaction):
-    output: str = ' , '.join(Options)
+    global optionMessage
+    if bool(Options):
+        optionMessage = 'Sorry, there are currently no restaurants being considered!'
 
-    await interaction.response.send_message(f'The current options to choose from are: ' + output)
+    await interaction.response.send_message(optionMessage)
 
 
 @bot.event
